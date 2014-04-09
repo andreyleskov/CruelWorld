2650922d8a2ff09d366b9e2874a5023f95f9ed4a
aleskov <andrey.leskov@gmail.cpm>
2014-04-09 22:27:17 +0400

    Predator ability and goblin and sheep classes

---
+++ CruelWorld/Abilities/PredatorAbility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CruelWorld.Abilities
+{
+    class PredatorAbility : IPredator
+    {
+        private readonly IEnumerable<ICreature> _owner;
+
+        private readonly HashSet<Type> _victims;
+
+        public PredatorAbility(IEnumerable<ICreature> owner, params Type[] victims)
+        {
+            this._owner = owner;
+            _victims = new HashSet<Type>(victims);
+        }
+
+        public bool CanEat(ICreature creature)
+        {
+            return _victims.Contains(creature.GetType());
+        }
+
+        public bool TryEat(params ICreature[] victims)
+        {
+            var feast = new Feast(_owner, victims.Where(v => _victims.Contains(v.GetType())));
+            return feast.Resolve();
+        }
+    }
+}
